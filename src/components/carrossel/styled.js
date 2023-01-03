ab2d8760373c1ce3930a2f63244da765286d6a6e
Leo Carrati <carrati3@gmail.com>
2023-01-03 19:19:59 -0300

    FIX Background nao aparecendo

--- src/components/carrossel/styled.js
+++ src/components/carrossel/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
     height: 300px;
     max-width: 1140px;
-    background-image: url(/hero/ksp.jpg);
+    background-image: url(./hero/ksp.jpg);
     background-size: 100% 100%;
     background-position: center;
 `
